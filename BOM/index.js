011e619d052db669618eab287d4eb65262a1d63c
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-29 12:55:22 +0000

    DOM-BOM

---
+++ BOM/index.js
@@ -0,0 +1,7 @@
+alert("Your JS ir running");
+let a = prompt("Enter a number: ");
+let bool = confirm("Should the number be displayed on screen: ");
+if(bool)
+document.write(a);
+else
+document.write("Null");
---
+++ DOM/index.js
@@ -0,0 +1,4 @@
+console.log(window);
+//window.console.log(window) is also valid
+
+console.log(document);
