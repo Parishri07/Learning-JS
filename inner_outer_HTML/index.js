a1718591d3edabbd0cc4f52e023072e9a0a488f1
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-29 18:06:25 +0000

    new

---
+++ inner_outer_HTML/index.js
@@ -0,0 +1,10 @@
+console.dir(document);
+//this prints the document with all the properties of it
+
+console.log(document);
+//this prints the document HTML
+
+console.log(document.body.firstElementChild.innerHTML);
+console.log(document.body.firstElementChild.outerHTML);
+
+console.log(document.body.textContent);
