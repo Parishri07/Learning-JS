f48657f9bf61bd80a1abe625ef378e3dbd08b43f
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-30 08:47:19 +0000

    asychronous and sychronous programming

---
+++ callbacks/index.js
@@ -0,0 +1,13 @@
+//Synchronous programming 
+// let a = prompt("What is your name: ");
+// let b = prompt("Age: ");
+// console.log("Hello "+a+"your age is: "+b);
+//order is execution is as specified
+
+//Asynchronous programming
+// console.log("Start");
+// setTimeout(function() {
+//     console.log("I am great")
+// },3000);
+// console.log("End");
+// initiated but finished later
