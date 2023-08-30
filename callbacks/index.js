fd22d75a6a51796f5b402528802634dfc12aac90
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-30 09:39:44 +0000

    callback

--- callbacks/index.js
+++ callbacks/index.js
@@ -7,7 +7,30 @@
 //Asynchronous programming
 // console.log("Start");
 // setTimeout(function() {
-//     console.log("I am great")
+//     console.log("I am great") 
 // },3000);
 // console.log("End");
 // initiated but finished later
+
+//Callbacks
+function loadScript(src, callback){
+    var script = document.createElement("script");
+    script.src = src;
+    script.onload = function(){
+        callback(null, src);
+    }
+    script.onerror = function(){
+        console.log("Error loading script");
+        callback(new Error("Src got some error"))
+    }
+    document.body.appendChild(script);
+}
+function hello(error, src){
+    if(error){
+        console.log(error)
+        return;
+    }
+    alert("hello "+src);
+}
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js", hello);
