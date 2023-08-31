57ccd6ce2c40b3d090e4663101f6ca6ac3a9cd5c
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-31 17:56:16 +0000

    promises

---
+++ promises/index.js
@@ -0,0 +1,19 @@
+let promise = new Promise(function(resolve, reject){
+    alert("Hello");
+    resolve(66);
+ })
+ 
+ console.log("Hello One");
+ setTimeout(()=>{
+     console.log("My name is Anthony Gonzalves Two");
+ },2000)
+ console.log("hello threee");
+ 
+ console.log(promise);
+ //prints that promise is resolved and fulfilled
+ 
+ //fetch google.com homepage => console.log("google.com homepage done")
+ //Fetch data from the data api
+ //Fetch pictures from server
+ //Print downloading
+ //Rest of the script
