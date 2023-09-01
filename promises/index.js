f8b0efb30185cb5f30b041fc218dd044fa4dd195
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-09-01 05:30:14 +0000

    changes promises

--- promises/index.js
+++ promises/index.js
@@ -1,15 +1,42 @@
-let promise = new Promise(function(resolve, reject){
-    alert("Hello");
-    resolve(66);
+//The promise constructor has two properties:
+//1.state: fulfilled or rejected
+//2.result: value when resolved or error when rejected
+
+let p1 = new Promise(function(resolve, reject){
+    console.log("Promise is pending")
+    setTimeout(()=>{
+     resolve(true);
+    },3000)
  })
  
- console.log("Hello One");
- setTimeout(()=>{
-     console.log("My name is Anthony Gonzalves Two");
- },2000)
- console.log("hello three");
+ let p2 = new Promise(function(resolve, reject){
+     console.log("Promise is pending")
+     setTimeout(()=>{
+      reject(new Error("I am an error"));
+     },3000)
+  })
  
- console.log(promise);
+ p1.then((value)=>{
+     console.log(value)
+ })
+ 
+ // p2.catch((error)=>{
+ //     console.log(error)
+ // })
+ 
+ p2.then((value)=>{
+     console.log(value)
+ }, (error)=>{
+     console.log(error)
+ })
+ 
+ // console.log("Hello One");
+ // setTimeout(()=>{
+ //     console.log("My name is Anthony Gonzalves Two");
+ // },2000)
+ // console.log("hello threee");
+ 
+ // console.log(promise);
  //prints that promise is resolved and fulfilled
  
  //fetch google.com homepage => console.log("google.com homepage done")
