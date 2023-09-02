f1d0cdfef6701579ff4d5f589358d6e82e4966ad
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-09-02 05:28:15 +0000

    try and catch

---
+++ try_catch/index.js
@@ -0,0 +1,24 @@
+setTimeout(()=>{
+    console.log("Hello");
+},2000)
+
+try{
+    console.log(kartik)
+}
+catch(error){
+    console.log(error);
+}
+//if exception happens in scheduled code like in setTimeout, then try.. catch wont catch it
+
+setTimeout(()=>{
+    console.log("Good morning");
+},2000)
+setTimeout(()=>{
+    console.log("Good afternoon");
+},2000)
+setTimeout(()=>{
+    console.log("Good evening");
+},2000)
+setTimeout(()=>{
+    console.log("Good night");
+},2000)
