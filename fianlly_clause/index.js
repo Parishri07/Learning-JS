24c781c31e4cbdd24cc1a87d923f44a86c9c15d0
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-09-02 12:53:37 +0000

    changes

--- fianlly_clause/index.js
+++ fianlly_clause/index.js
@@ -1,12 +1,16 @@
 try{
     let a = 0;
-    console.log(t)
+    
+    console.log(time);
+    //this gives an error
 }
 catch(err){
     console.log(err);
+
     console.log(hello);
+    //this statament also gives an error
 }
 finally{
     console.log("Hello world");
 }
-// finally clause runs even if the catch block has error
+// finally clause runs even if the catch block has error or there is a return statement in try block
