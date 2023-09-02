038745a4be039a59c43f60e91b348b295fafe8a0
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-09-02 09:46:58 +0000

    finally clause

---
+++ fianlly_clause/index.js
@@ -0,0 +1,12 @@
+try{
+    let a = 0;
+    console.log(t)
+}
+catch(err){
+    console.log(err);
+    console.log(hello);
+}
+finally{
+    console.log("Hello world");
+}
+// finally clause runs even if the catch block has error
