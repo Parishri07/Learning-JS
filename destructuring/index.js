736c973b1ba00ec0f66bb974d03c1a1fc76b3a0d
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-10-17 17:39:42 +0530

    Add files via upload

---
+++ destructuring/index.js
@@ -0,0 +1,11 @@
+let arr = [3, 4, 7, 9, 12]
+
+// let [a, b, c, ...rest] = arr
+//no need for separating differently
+// console.log(a, b, c, rest)
+
+// let [a, ,b, ...rest] = arr
+// console.log(a, b, rest)
+
+const {a, b} = {a:1, b:2}
+console.log(a,b)
