901b0e5ebd938cc59cb2fb87350bdb2745e01341
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-01-02 16:09:00 +0530

    Update 04_passwordValidator.js

--- Javscript interview questions/Solutions/04_passwordValidator.js
+++ Javscript interview questions/Solutions/04_passwordValidator.js
@@ -1,27 +1,15 @@
 // The Password Validator: You are building a password validation feature. Create a function that checks if a given password meets the following criteria: at least 8 characters long, contains both uppercase and lowercase letters, and includes at least one digit.
 
-let pswd = "MyNameIs1234";
-let l = pswd.length;
+let password = "MyNameIs1234";
 
-if(l >= 8){
-    let ctr = 0;
+const hasUppercase = /[A-Z]/.test(password); 
+const hasLowercase = /[a-z]/.test(password); 
+const hasDigit = /\d/.test(password); 
+const isLengthValid = password.length >= 8;
 
-    for(let i=0; i<l; i++){
-        let c = pswd.charCodeAt(i);
-        let n = parseInt(c);
+if(hasDigit && hasLowercase && hasUppercase && isLengthValid)
+ console.log("Valid");
+else 
+ console.log("Invalid");
 
-        if((n>=48) && (n<=57))
-         ctr++;
-        if((n>=65) && (n<=90))
-         ctr++;
-        if((n>=97) && (n<=122))
-         ctr++;
-    }
-
-    if(ctr >= 3){
-        console.log("Valid");
-    }
-    else{
-        console.log("Invalid");
-    }
-}
+// Regular Expressions: /[A-Z]/, /[a-z]/, /\d/
